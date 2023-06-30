f0e98f92030ad526dca7a1a985ff74748a14815f
dapoorking <muslimkolachi@gmail.com>
2023-07-01 01:56:52 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/fitnessnearme/events.java
@@ -0,0 +1,41 @@
+package com.example.fitnessnearme;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class events extends AppCompatActivity {
+
+
+    ImageView facebookIcon;
+    ImageView twitterIcon;
+    ImageView instagramIcon;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+        facebookIcon = findViewById(R.id.facebookIcon);
+        twitterIcon = findViewById(R.id.twitterIcon);
+        instagramIcon = findViewById(R.id.instagramIcon);
+    }
+    public void onFacebookIconClicked(View view) {
+        String facebookUrl = "https://www.youtube.com/watch?v=5T1_PWX6odY";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+        startActivity(intent);
+    }
+
+    public void onTwitterIconClicked(View view) {
+        String twitterUrl = "https://www.youtube.com/watch?v=5upQGvf22qA";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(twitterUrl));
+        startActivity(intent);
+    }
+
+    public void onInstagramIconClicked(View view) {
+        String instagramUrl = "https://www.youtube.com/watch?v=o6jZ7IFt6mQ";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(instagramUrl));
+        startActivity(intent);
+    }
+}
